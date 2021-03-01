c7b8740ef295f84b920dd64feae890972e899361
SanaeAbm <sanae.abm@gmail.com>
2021-03-01 21:14:58 +0100

    added navbar

--- my_shiny/app.R
+++ my_shiny/app.R
@@ -13,11 +13,13 @@ list_choices <-  unique(msleep$vore)[!is.na(unique(msleep$vore))]
 names(list_choices) <- paste(unique(msleep$vore)[!is.na(unique(msleep$vore))],"vore",sep="")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- navbarPage("Shiny app",
+    tabPanel("msleep",
+    fluidPage(
 
     # Application title
-    titlePanel("This is a new shiny app"),
-    includeMarkdown("references.md"),
+    
+   
     sidebarLayout(
         sidebarPanel(
             selectInput("select", label = h3("Plot by type of alimentation"), 
@@ -27,9 +29,10 @@ ui <- fluidPage(
         mainPanel(
             plotOutput(outputId = "plot")
         )
-    )
+))),
+tabPanel("References",
+         includeMarkdown("references.md"))
 )
-
 col_scale <- scale_colour_discrete(limits = list_choices)
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
