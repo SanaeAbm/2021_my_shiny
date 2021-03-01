82549438987e340015608e3fb7a8afb1bab25189
SanaeAbm <sanae.abm@gmail.com>
2021-03-01 20:41:08 +0100

    plot filterred by vore variables

--- my_shiny/app.R
+++ my_shiny/app.R
@@ -9,6 +9,8 @@
 
 library(shiny)
 library(tidyverse)
+list_choices <-  unique(msleep$vore)[!is.na(unique(msleep$vore))]
+names(list_choices) <- paste(unique(msleep$vore)[!is.na(unique(msleep$vore))],"vore",sep="")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -16,15 +18,20 @@ ui <- fluidPage(
     # Application title
     titlePanel("This is a new shiny app"),
     includeMarkdown("references.md"),
+    selectInput("select", label = h3("Plot by type of alimentation"), 
+                choices = list_choices,
+                selected = 1),
     h3("plots"),
     plotOutput(outputId = "plot")
     )
 
+col_scale <- scale_colour_discrete(limits = list_choices)
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     output$plot <- renderPlot({
-        ggplot(msleep, aes(bodywt, sleep_total, colour = vore)) +
+        ggplot(msleep %>% filter(vore == input$select), aes(bodywt, sleep_total, colour = vore)) +
             scale_x_log10() +
+            col_scale+
             geom_point() + facet_wrap(~ vore, nrow = 2)
     })
     
