0ad97a38f84bec00e3cf98ab4d887638d95b0c6e
SanaeAbm <sanae.abm@gmail.com>
2021-03-08 11:06:55 +0100

    disable enable bins

--- my_shiny/app.R
+++ my_shiny/app.R
@@ -9,6 +9,8 @@
 
 library(shiny)
 library(tidyverse)
+library(shinyjs)
+
 list_choices <-  unique(msleep$vore)[!is.na(unique(msleep$vore))]
 names(list_choices) <- paste(unique(msleep$vore)[!is.na(unique(msleep$vore))],"vore",sep="")
 
@@ -39,9 +41,16 @@ tabPanel("Random generator",
                                        selected = 1), #default the frist one is the selected
                            #Slider: que se deliza paa elegir la opción 
                            sliderInput("n_sample", label = h3("Number of samples"), 
-                                       min = 10, max = 100, value = 50), #max and minm value del slider; value="50" ( es el current vaue, valor inicial)
-                           sliderInput("n_bins", label = h3("Number of bins"), min = 1, 
-                                       max = 50, value = 30)
+                                       min = 10, max = 100, value = 50), #max and minm value del slider; value="50" ( es el current value, valor inicial)
+                           fluidRow( #page that is 12 columns
+                               h3(style = "margin-left: 20px; margin-bottom: 0px;", "Number of bins"),
+                               column(2,#columns to do:
+                                      div(style = "margin-top: 37px", checkboxInput("auto_bins", label = "auto", value = TRUE))
+                               ),
+                               column(10,#10 columns to do:
+                                      sliderInput("n_bins", label="", min = 1, max = 50, value = 30)
+                               )
+                           )
                        ), # sidebarPanel
                        mainPanel(
                            #Create a div with nothing inside --> server side that creates the content
@@ -59,8 +68,17 @@ tabPanel("References",
              div(class="card card-body",
                  includeMarkdown("references.md")
              ))
-))
+),
+useShinyjs()#inject the java srcript library n your html file
+#Now, shiny app is using some inyect js
+
+
+
+)
 col_scale <- scale_colour_discrete(limits = list_choices)
+
+
+
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
     
@@ -81,7 +99,21 @@ server <- function(input, output, session) {
     # You want to "eval" it --> obtaining 20 normal objects (rnorm(20))--> what you want to plot
     #Since cmd comes from a reactive values is not a value is a function
     cmd = reactive(eval(parse(text=paste(input$dist,"(",input$n_sample,")",sep=""))));
-    output$pulpo<-renderPlot(hist(cmd()))
+    
+    #We need observe because the element "input$ato_bins" is a function: changes with user interaction
+    #Observe is a reactive function that can get value that input gives to it
+    #Whenever auto_bins is changed, this fuunction is called
+    #Observe doesnt return any value
+    #when auto_bins is true we desable the n_bins
+    #Css class: disable and enable --> in the shinyjs()
+    observe(if(input$auto_bins) disable("n_bins") else enable("n_bins") )
+    
+    #If the botton is pressed: plots hist w/out bins
+    #Otherwise, if clicked, give number of bins:
+    output$pulpo <- renderPlot(
+        if(input$auto_bins) hist(cmd()) 
+        else hist(cmd(), breaks=input$n_bins)
+    );
 }
 
 
